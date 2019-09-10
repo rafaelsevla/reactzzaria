2baec96624070fef7d6e12b10325abf0ea0003e5
Rafael Costa <rafaelsevla@gmail.com>
2019-09-10 20:39:59 -0300

    defined initial routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { MainPage } from './pages/main'
+import { Login } from './pages/login'
 
-const App = () => <h1>App</h1>
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route path='/' component={MainPage} exact />
+      <Route path='/login' component={Login} />
+    </Switch>
+  </BrowserRouter>
+)
 
 export default App
---
+++ src/pages/main/main.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Main = () => <h1>main</h1>
+
+export default Main
