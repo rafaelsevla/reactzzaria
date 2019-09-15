0f66baeec77930c9ead59394dff4e5d4521dbde8
Rafael Costa <rafaelsevla@gmail.com>
2019-09-15 13:36:50 -0300

    styling logo

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import styled from 'styled-components'
 import {
   AppBar,
   IconButton,
@@ -13,17 +14,37 @@ import { ReactComponent as MainLogo } from 'pages/login/logo-react-zzaria.svg'
 const Main = () => (
   <AppBar>
     <Toolbar>
-      <MainLogo />
+      <LogoContainer>
+        <Logo />
+      </LogoContainer>
+
       <Typography color='inherit'>Olá João =)</Typography>
       <IconButton color='inherit'>
         <AccountCircle />
       </IconButton>
 
-      <Menu open>
+      <Menu open={false}>
         <MenuItem>Sair</MenuItem>
       </Menu>
     </Toolbar>
   </AppBar>
 )
+
+const LogoContainer = styled.div`
+  flex-grow: 1;
+`
+
+const Logo = styled(MainLogo)`
+  height: 50px;
+  width: 200px;
+
+  & path {
+    fill: #fff;
+  }
+
+  & line {
+    stroke: #fff;
+  }
+`
 
 export default Main
