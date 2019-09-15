200c714592fbdc2a3849d12aa81609c98b67e7e9
Rafael Costa <rafaelsevla@gmail.com>
2019-09-15 15:20:41 -0300

    isolate component header

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,51 +1,22 @@
-import React, { useState, useContext } from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import {
-  AppBar,
   Divider as MaterialDivider,
   Grid,
-  IconButton,
-  Menu,
-  MenuItem,
   Paper,
-  Toolbar as MaterialToolbar,
   Typography,
   withStyles
 } from '@material-ui/core'
-import { AccountCircle } from '@material-ui/icons'
-import { ReactComponent as MainLogo } from 'images/logo-react-zzaria.svg'
+import Header from './header'
 import { AuthContext } from 'contexts/auth'
 
 const Main = () => {
-  const [anchorElement, setAnchorElement] = useState(null)
-  const { logout, userInfo } = useContext(AuthContext)
+  const { userInfo } = useContext(AuthContext)
   const userName = userInfo.user.displayName.split(' ')[0]
-
-  const handleOpenMenu = e => setAnchorElement(e.target)
-  const handleClose = () => setAnchorElement(null)
 
   return (
     <>
-      <AppBar>
-        <Toolbar>
-          <LogoContainer>
-            <Logo />
-          </LogoContainer>
-
-          <Typography color='inherit'>Olá {userName} =)</Typography>
-          <IconButton color='inherit' onClick={handleOpenMenu}>
-            <AccountCircle />
-          </IconButton>
-
-          <Menu
-            open={!!anchorElement}
-            onClose={handleClose}
-            anchorEl={anchorElement}
-          >
-            <MenuItem onClick={logout}>Sair</MenuItem>
-          </Menu>
-        </Toolbar>
-      </AppBar>
+      <Header />
 
       <Spacer />
 
@@ -117,23 +88,6 @@ const Divider = styled(MaterialDivider)`
   width: 100%;
 `
 
-const LogoContainer = styled.div`
-  flex-grow: 1;
-`
-
-const Logo = styled(MainLogo)`
-  height: 50px;
-  width: 200px;
-
-  & path {
-    fill: #fff;
-  }
-
-  & line {
-    stroke: #fff;
-  }
-`
-
 const PaperPizza = styled(Paper)`
   align-items: center;
   display: flex;
@@ -202,11 +156,5 @@ const Title = styled(Typography).attrs({
   gutterBottom: true,
   align: 'center'
 })``
-
-const Toolbar = styled(MaterialToolbar)`
-  margin: 0 auto;
-  max-width: 960px;
-  width: 100%;
-`
 
 export default Main
