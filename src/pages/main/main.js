e92ddb9cee4f76899cf1dfd645588d61794440a6
Rafael Costa <rafaelsevla@gmail.com>
2019-09-15 14:39:24 -0300

    markup itens of main page

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,11 +2,14 @@ import React, { useState, useContext } from 'react'
 import styled from 'styled-components'
 import {
   AppBar,
+  Grid,
   IconButton,
   Menu,
   MenuItem,
+  Paper,
   Toolbar as MaterialToolbar,
-  Typography
+  Typography,
+  withStyles
 } from '@material-ui/core'
 import { AccountCircle } from '@material-ui/icons'
 import { ReactComponent as MainLogo } from 'images/logo-react-zzaria.svg'
@@ -15,35 +18,90 @@ import { AuthContext } from 'contexts/auth'
 const Main = () => {
   const [anchorElement, setAnchorElement] = useState(null)
   const { logout, userInfo } = useContext(AuthContext)
+  const userName = userInfo.user.displayName.split(' ')[0]
 
   const handleOpenMenu = e => setAnchorElement(e.target)
   const handleClose = () => setAnchorElement(null)
 
   return (
-    <AppBar>
-      <Toolbar>
-        <LogoContainer>
-          <Logo />
-        </LogoContainer>
+    <>
+      <AppBar>
+        <Toolbar>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
 
-        <Typography color='inherit'>
-          Olá {userInfo.user.displayName.split(' ')[0]} =)
-        </Typography>
-        <IconButton color='inherit' onClick={handleOpenMenu}>
-          <AccountCircle />
-        </IconButton>
+          <Typography color='inherit'>Olá {userName} =)</Typography>
+          <IconButton color='inherit' onClick={handleOpenMenu}>
+            <AccountCircle />
+          </IconButton>
 
-        <Menu
-          open={!!anchorElement}
-          onClose={handleClose}
-          anchorEl={anchorElement}
-        >
-          <MenuItem onClick={logout}>Sair</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+          <Menu
+            open={!!anchorElement}
+            onClose={handleClose}
+            anchorEl={anchorElement}
+          >
+            <MenuItem onClick={logout}>Sair</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+
+      <Spacer />
+
+      <Content>
+        <Grid container direction='column' alignItems='center'>
+          <Typography variant='h3'>
+            O que vai ser hoje {userName}? =)
+          </Typography>
+
+          <Typography variant='h4'>Escolha o tamanho da pizza:</Typography>
+        </Grid>
+
+        <Grid container spacing={16}>
+          {pizzaSizes.map(pizza => (
+            <Grid item key={pizza.id} xs={4}>
+              <Paper style={{ padding: 20 }}>
+                <div>{pizza.size}cm</div>
+                <Typography>{pizza.name}</Typography>
+                <Typography>
+                  {pizza.slices} fatias, {pizza.flavours} sabores
+                </Typography>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Content>
+    </>
   )
 }
+
+const pizzaSizes = [
+  {
+    id: 0,
+    name: 'Pequena',
+    size: 28,
+    slices: 2,
+    flavours: 1
+  },
+  {
+    id: 1,
+    name: 'Média',
+    size: 30,
+    slices: 6,
+    flavours: 2
+  },
+  {
+    id: 2,
+    name: 'Grande',
+    size: 32,
+    slices: 8,
+    flavours: 3
+  }
+]
+
+const Content = styled.main`
+  padding: 20px;
+`
 
 const LogoContainer = styled.div`
   flex-grow: 1;
@@ -61,6 +119,12 @@ const Logo = styled(MainLogo)`
     stroke: #fff;
   }
 `
+
+const style = theme => ({ main: theme.mixins.toolbar })
+
+const Spacer = withStyles(style)(({ classes }) => (
+  <div className={classes.main} />
+))
 
 const Toolbar = styled(MaterialToolbar)`
   margin: 0 auto;
